28de764010ac891eb8b64dd78b0fea7498a8bc7c
LAMNAZZAH <medlmz01@gmail.com>
2022-05-14 23:15:12 +0100

    post /products

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -4,20 +4,31 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
 router.get('/', async (req, res, next) => {
-  try {
-    const products = await prisma.product.findMany({});
-    res.json(products)
-  } catch (error) {
-    next(error)
-  }
+ 
 });
 
 router.get('/products', async (req, res, next) => {
-  res.send({})
+  try {
+    const products = await prisma.product.findMany({
+      include: { category: true }
+    });
+    const categories = await prisma.category.findMany({})
+    res.json({products, categories})
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/products', async (req, res, next) => {
-  res.send({})
+  try {
+    const { name, price, categoryId } = req.body 
+    const createProduct = await prisma.product.create({
+      data: { name, price, categoryId }
+    });
+    res.json(createProduct)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/products/:id', async (req, res, next) => {
